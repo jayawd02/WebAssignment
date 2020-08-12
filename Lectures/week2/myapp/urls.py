8ea9157ea5025ecf5821f70bacd45eb5c83b88c2
Dinusha <jayawd02@myunitec.ac.nz>
2020-08-12 21:46:12 +1200

    views and templates updated

--- Lectures/week2/myapp/urls.py
+++ Lectures/week2/myapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:course_id>/', views.detail, name='detail'),
+    path('<int:course_id>/timetable/', views.timetable, name='timetable'),
+    path('<int:course_id>/enrollments/', views.enrollments, name='enrollments'),
 ]
--- Lectures/week2/myapp/views.py
+++ Lectures/week2/myapp/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from myapp.models import Course, TimeTable, Student
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    course_list = Course.objects.all()
+    context = { 'course_list': course_list}
+    return render(request, 'myapp/index.html', context)
+
+def detail (request,course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'myapp/detail.html', {'course': course})
+
+
+def timetable(request,course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'myapp/timetable.html', {'course': course})
+
+
+def enrollments (request,course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    students = course.enrollment
+    return render(request, 'myapp/enrollments.html', {'course': course,'students': students})
