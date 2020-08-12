4ca1f5351df742a89e370276c60c699af6bdba60
Dinusha <jayawd02@myunitec.ac.nz>
2020-08-12 14:24:48 +1200

    commit 2

--- Lectures/week2/myapp/models.py
+++ Lectures/week2/myapp/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    birth_date = models.DateTimeField('date of birth')
+    birth_date = models.DateField('date of birth')
+
+    def __str__(self):
+        return self.first_name
 
 class Course(models.Model):
     course_name = models.CharField(max_length=250)
@@ -12,10 +15,15 @@ class Course(models.Model):
     course_Description = models.TextField()
     credits = models.IntegerField()
     enrollment = models.ManyToManyField(Student)
+    def __str__(self):
+        return self.course_name
+
 
 class TimeTable(models.Model):
-    class_date = models.DateTimeField()
-    class_startTime = models.DateTimeField()
-    class_endTime = models.DateTimeField()
+    class_date = models.DateField()
+    class_startTime = models.TimeField()
+    class_endTime = models.TimeField()
     class_room = models.CharField(max_length=50)
     course_id = models.ForeignKey(Course,on_delete=models.CASCADE)
+
+
