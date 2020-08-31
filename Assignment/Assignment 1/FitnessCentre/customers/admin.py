799ce7aa53b7bcde8ae4334186bd16c0e2d9a2a7
Dinusha <jayawd02@myunitec.ac.nz>
2020-09-01 01:53:37 +1200

    customer model and test view

--- Assignment/Assignment 1/FitnessCentre/customers/admin.py
+++ Assignment/Assignment 1/FitnessCentre/customers/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from customers.models import Customer
 
-# Register your models here.
+
+# Define an inline admin descriptor for Customer model
+# which acts a bit like a singleton
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    can_delete = False
+    verbose_name_plural = 'employee'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (CustomerInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- Assignment/Assignment 1/FitnessCentre/customers/models.py
+++ Assignment/Assignment 1/FitnessCentre/customers/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     address_line1 = models.CharField(max_length=50)
@@ -15,7 +17,7 @@ class Customer(models.Model):
     allergies = models.TextField(blank=True, null=True)
     MealPreference = models.TextChoices('MealPreference', 'Vegan Vegetarian Pescatarian')
     meal_preference = models.CharField(blank=True, null=True, choices=MealPreference.choices, max_length=12)
-    height = models.DecimalField()
+    height = models.DecimalField(decimal_places=2,max_digits=5)
     photo = models.ImageField(blank=True, null=True)
     Gender = models.TextChoices('Gender', 'Male Female Other')
     gender = models.CharField(blank=True, null=True, choices=Gender.choices, max_length=6)
