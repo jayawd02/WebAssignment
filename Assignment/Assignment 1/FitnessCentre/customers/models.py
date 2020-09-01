cc264a61622e9ca44df525e9614146c88a72ff57
Dinusha <jayawd02@myunitec.ac.nz>
2020-09-02 01:49:54 +1200

    added membership plan model

--- Assignment/Assignment 1/FitnessCentre/customers/models.py
+++ Assignment/Assignment 1/FitnessCentre/customers/models.py
@@ -16,7 +16,17 @@ class Customer(models.Model):
     allergies = models.TextField(blank=True, null=True)
     MealPreference = models.TextChoices('MealPreference', 'Vegan Vegetarian Pescatarian')
     meal_preference = models.CharField(blank=True, null=True, choices=MealPreference.choices, max_length=12)
-    height = models.DecimalField(decimal_places=2,max_digits=5)
+    height = models.DecimalField(decimal_places=2, max_digits=5)
     photo = models.ImageField(blank=True, null=True)
     Gender = models.TextChoices('Gender', 'Male Female Other')
     gender = models.CharField(blank=True, null=True, choices=Gender.choices, max_length=6)
+
+
+class MembershipPlan(models.Model):
+    plan_name = models.CharField(max_length=25)
+    PlanType = models.TextChoices('PlanType', 'Individual Group Couple Family Corporate')
+    plan_type = models.CharField(choices=PlanType.choices, max_length=12)
+    duration = models.DurationField(name='duration (months)') #todo change the type
+    fee = models.DecimalField(decimal_places=2, max_digits=10)
+    ActiveStatus=models.TextChoices('ActiveStatus' , 'Active Inactive')
+    status = models.CharField(choices=ActiveStatus.choices, default='Active', max_length=10)
