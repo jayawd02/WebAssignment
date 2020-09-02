8ae03601de9c878e4df2936b044f4633a79915e0
Dinusha <jayawd02@myunitec.ac.nz>
2020-09-02 16:08:15 +1200

    Membership model,changes to membershiplans

--- Assignment/Assignment 1/FitnessCentre/customers/admin.py
+++ Assignment/Assignment 1/FitnessCentre/customers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer, MembershipPlan
+from .models import Customer, MembershipPlan, Membership
 
 admin.site.register(Customer)
 admin.site.register(MembershipPlan)
+admin.site.register(Membership)
--- Assignment/Assignment 1/FitnessCentre/customers/models.py
+++ Assignment/Assignment 1/FitnessCentre/customers/models.py
@@ -21,12 +21,28 @@ class Customer(models.Model):
     Gender = models.TextChoices('Gender', 'Male Female Other')
     gender = models.CharField(blank=True, null=True, choices=Gender.choices, max_length=6)
 
+    def __str__(self):
+        return self.first_name
+
 
 class MembershipPlan(models.Model):
     plan_name = models.CharField(max_length=25)
     PlanType = models.TextChoices('PlanType', 'Individual Group Couple Family Corporate')
     plan_type = models.CharField(choices=PlanType.choices, max_length=12)
-    duration = models.DurationField(name='duration (months)') #todo change the type
+    DurationChioces = models.TextChoices('DurationChoice', '1-month 3-months 6-months 1-year')
+    duration = models.CharField(choices=DurationChioces.choices, max_length=10)
     fee = models.DecimalField(decimal_places=2, max_digits=10)
-    ActiveStatus=models.TextChoices('ActiveStatus' , 'Active Inactive')
+    ActiveStatus = models.TextChoices('ActiveStatus', 'Active Inactive')
     status = models.CharField(choices=ActiveStatus.choices, default='Active', max_length=10)
+
+    def __str__(self):
+        return self.plan_name
+
+
+class Membership(models.Model):
+    membership_plan = models.ForeignKey(MembershipPlan, on_delete=models.PROTECT)
+    customers=models.ManyToManyField(Customer)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    discount = models.DecimalField("discount percentage", decimal_places=2, max_digits=5, default=0)
+    final_fee = models.DecimalField(decimal_places=2, max_digits=10)
